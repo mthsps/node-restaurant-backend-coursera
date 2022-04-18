1d4f27fe639e8b4022444e47883115ec6a713a4c
Matheus <dmatheusps@gmail.com>
2022-04-18 15:42:11 -0300

    Mongoose: Sub-documents to a Document

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -15,11 +15,27 @@ connect.then((db) => {
     })
         .then((dish) => {
             console.log(dish);
-            return Dishes.find({});
+            return Dishes.findByIdAndUpdate(dish._id, {
+                $set: { description: 'Update test'}
+            },{
+                new: true
+            })
+            .exec();
         })
-        .then((dishes) => {
-            console.log(dishes);
+        .then((dish) => {
+            console.log(dish);
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling',
+                author: 'Leonardo di Carpaccio'
+            });
+
+            return dish.save();
+        })
+        .then((dish) => {
+            console.log(dish);
             return Dishes.deleteOne({});
+            
         })
         .then(() => {
             return mongoose.connection.close();
